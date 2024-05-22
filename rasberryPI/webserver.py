7d8288c2f87aaa0b27973ec29ae27395e8965e60
MaxZ <mxjhfr@gmx.de>
2024-05-22 11:53:33 +0200

    cleaned up

--- rasberryPI/webserver.py
+++ rasberryPI/webserver.py
@@ -1,7 +1,40 @@
 import requests
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import time
 
-url = 'http://192.168.178.63:5000'
-data = {'type': 'minecraft', 'value': 'done'}
 
-response = requests.post(url, json=data)
-print(response.json())
+def send_message(type, value):
+    url = 'http://192.168.178.63:5000'
+    data = {'type': type, 'value': value}
+
+    response = requests.post(url, json=data)
+    return response
+
+class RFID():
+    def __init__(self):
+        GPIO.setwarnings(False)
+
+    def readRFID(self):
+        reader = SimpleMFRC522()
+
+        try:
+            reader.read()
+            response = send_message("rfid", "scanned").json()
+            print("rread rfid tag.... sending message to flask")
+            print("sent")
+            GPIO.cleanup()
+            return response
+        except Exception as e:
+            print(f"Read Error: {e}")
+        GPIO.cleanup
+
+rfid = RFID()
+while True:
+    response = rfid.readRFID()
+    print(response)
+    time.sleep(5)
+
+
+
+
